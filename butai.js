c19a60e93c3a5e556f0e84d8b34b4aaeedb6ec25
gilboonet <gilbertfd@gmail.com>
2021-01-16 11:22:34 +0100

    simplification, commentaires

--- butai.js
+++ butai.js
@@ -1,3 +1,8 @@
+// Generateur de gabarit de BUTAI 
+// par Gilbert Duval, 2021
+// https://jscad.xyz/#https://raw.githubusercontent.com/gilboonet/designs/master/butai.js
+
+// Dependences
 const jscad = require('@jscad/modeling')
 const { rectangle, cuboid, polygon } = jscad.primitives
 const { intersect, subtract, union } = jscad.booleans
@@ -5,6 +10,7 @@ const { translate, translateX, translateY, translateZ, mirrorX, mirrorY, mirrorZ
 const { extrudeLinear } = jscad.extrusions
 const { colorize, colorNameToRgb } = jscad.colors
 
+// Parametres
 const getParameterDefinitions = () => {
   return [
     {name: 'largeur', caption: 'Largeur:', type: 'float', initial: 39},
@@ -19,27 +25,33 @@ const getParameterDefinitions = () => {
   ]
 }
 
+// BUTAI = cadre ouvert sur l'avant/l'arriere/la gauche/la droite
+//         + jeu de 3 volets se refermant sur l'avant
+
+// ELEMENTS : ecran avant/arriere - entretoise gauche/droite
+//						+ volet gauche/central/droite
+ 
+// Code
 const main = (params) => {
-  const epaisseurCarton = 0.6
-  
+
+  function extrudeCarton(geom){ return extrudeLinear({height: params.epaisseurCarton}, geom) }
+  function colorie(couleur, geom) { return colorize(colorNameToRgb(couleur), geom) }
+
   const largTotale = params.largeur + params.epaisseurEcran * 2 + params.epaisseurCarton * 2
   const hautTotale = params.hauteur + params.epaisseurEcran + params.epaisseurHaut + params.epaisseurCarton * 2
-  
-  function extrudeCarton(geom){ return extrudeLinear({height: params.epaisseurCarton}, geom) }
 
   let ecran = translateY( (params.epaisseurEcran - params.epaisseurHaut)/2, rectangle({size: [params.largeur, params.hauteur]}))
   let cadre = rectangle({size: [largTotale, hautTotale]})
   let avant = subtract(cadre, ecran)
-  let plaqueAvant = extrudeCarton(avant)
-  
+  let plaqueAvant = extrudeCarton(avant)  
   let entretoise = cuboid({size:[largTotale, params.epaisseurCarton, params.profondeur]})
      
   const x0 = -largTotale /2
-  const x2 =  largTotale /2
-  const x1 = (x0 + x2) / 2
+  const x2 = -x0
+  const x1 = 0
   
   const y0 = -hautTotale /2
-  const y2 =  hautTotale /2
+  const y2 = -y0
   const y1 = y0 + (y2 - y0) * params.ratioPtCentral 
   
   let pH = polygon({points: [ [x1, y1], [x2, y2], [x0,y2] ]})
@@ -51,32 +63,27 @@ const main = (params) => {
   let plaqueG = translateZ(decalage, extrudeCarton(pG))
   let plaqueD = translateZ(decalage, extrudeCarton(pD))
 
-  console.log('=>',params.etatFenetre)
   if((params.etatFenetre == 1)&&(params.mode == 3)){
-		plaqueH = translate([0,hautTotale,-params.epaisseurCarton], mirrorY(plaqueH))
-		plaqueG = translate([-largTotale,0,-params.epaisseurCarton], mirrorX(plaqueG))
-		plaqueD = translate([largTotale,0,-params.epaisseurCarton], mirrorX(plaqueD))
+		plaqueH = translate([0, hautTotale, -params.epaisseurCarton], mirrorY(plaqueH))
+		plaqueG = translate([-largTotale, 0,-params.epaisseurCarton], mirrorX(plaqueG))
+		plaqueD = translate([largTotale, 0, -params.epaisseurCarton], mirrorX(plaqueD))
 	}
   
   let r = []
-  	if(params.mode == 3){
-		r.push(colorize(colorNameToRgb('tan'), translateZ(- params.profondeur/2, plaqueAvant)))
-		r.push(colorize(colorNameToRgb('tan'), translateZ(  params.profondeur/2, plaqueAvant)))
-		r.push(colorize(colorNameToRgb('orange'), translate([0,  hautTotale/2 - params.epaisseurCarton/2, 0], entretoise)))
-		r.push(colorize(colorNameToRgb('orange'), translate([0, -hautTotale/2 + params.epaisseurCarton/2, 0], entretoise)))
-  
-		r.push(colorize(colorNameToRgb('yellow'), plaqueH))
-		r.push(colorize(colorNameToRgb('Green'), plaqueG))
-		r.push(colorize(colorNameToRgb('Maroon'), plaqueD))
+  if(params.mode == 3){
+		r.push(colorie('tan', translateZ(- params.profondeur/2, plaqueAvant)))
+		r.push(colorie('tan', translateZ(  params.profondeur/2, plaqueAvant)))
+		r.push(colorie('orange', translateY(hautTotale/2 - params.epaisseurCarton/2, entretoise)))
+		r.push(colorie('orange', translateY(-hautTotale/2 + params.epaisseurCarton/2, entretoise)))  
+		r.push(colorie('yellow', plaqueH))
+		r.push(colorie('Green', plaqueG))
+		r.push(colorie('Maroon', plaqueD))
 	} else {
 		r.push(translateY(hautTotale/2+ params.profondeur/2, rectangle({size: [largTotale, 4]})))
 		r.push(avant)
 		r.push(translateY(-hautTotale/2- params.profondeur/2, rectangle({size: [largTotale, 4]})))
-		r.push(translateY(-hautTotale- params.profondeur, avant))
-		
-		decalage = -hautTotale*2 - params.profondeur/2*2
+		r.push(translateY(-hautTotale- params.profondeur, avant))		
 		r.push(translateY(-hautTotale*2 - params.profondeur/2*2, pH))
-		
 		r.push(translate([-largTotale,-hautTotale*1 - params.profondeur/2*2], mirrorY(mirrorX(pG))))
 		r.push(translate([largTotale,-hautTotale*1 - params.profondeur/2*2], mirrorY(mirrorX(pD))))
 	}
