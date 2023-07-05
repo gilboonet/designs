ccde70a5925ac47e8d2f418b8e51504121f5078d
gilboonet <gilbertfd@gmail.com>
2023-07-05 10:00:21 +0200

    Update boiteSimple.js

--- 2023/boiteSimple.js
+++ 2023/boiteSimple.js
@@ -1,18 +1,29 @@
 const { cuboid } = require('@jscad/modeling').primitives
- 
-const main = () => {
-  let A = cuboid({size:[100, 100, 50]})
+
+const getParameterDefinitions = () => {
+    return [
+        { name: 'longueur', type: 'int', initial: 100, caption: 'Longueur ?' }, 
+        { name: 'largeur', type: 'int', initial: 100, caption: 'Largeur ?' },
+        { name: 'hauteur', type: 'int', initial: 50, caption: 'Hauteur ?' },
+    ]
+}
+
+const main = (params) => {
+  
+  let A = cuboid({size:[params.longueur, params.largeur, params.hauteur]})
   
   const cBlue = [0,0,1]
   const cGreen = [0,1,0]
   
+  // initialise les couleurs en BLEU
   for (i = 0; i < A.polygons.length; i++) {
     A.polygons[i].color = cBlue
   }
   
+  // met la face du haut en VERT
   A.polygons[5].color = cGreen
   
   return A 
 }
  
-module.exports = { main }
+module.exports = { getParameterDefinitions, main }
